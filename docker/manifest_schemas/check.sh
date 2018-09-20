a9f51630ad7363b3dfaa0b45eccd96bbc1a26cf2
Chen Shou <cshou@google.com>
2018-09-20 12:59:58 -0700

    Add static image

--- docker/manifest_schemas/check.sh
+++ docker/manifest_schemas/check.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-skopeo --insecure-policy copy --format=v2s1 docker://$SRC_REPO/${IMG_NAME} dir:${IMG_NAME}_v2s1
-skopeo --insecure-policy copy --format=v2s2 docker://$SRC_REPO/${IMG_NAME} dir:${IMG_NAME}_v2s2
-skopeo --insecure-policy copy --format=oci docker://$SRC_REPO/${IMG_NAME} dir:${IMG_NAME}_oci
+RET=0
 
-skopeo --insecure-policy copy --dest-creds=$DST_USER:$DST_PWD dir:${IMG_NAME}_v2s1 docker://$DST_REPO/${IMG_NAME}:v2s1
-skopeo --insecure-policy copy --dest-creds=$DST_USER:$DST_PWD dir:${IMG_NAME}_v2s2 docker://$DST_REPO/${IMG_NAME}:v2s2
-skopeo --insecure-policy copy --dest-creds=$DST_USER:$DST_PWD dir:${IMG_NAME}_oci docker://$DST_REPO/${IMG_NAME}:oci
+echo Pushing v2s1 image ${DST_REPO}/alpine:v2s1
+skopeo --insecure-policy copy --dest-tls-verify=false --dest-creds=${DST_USER}:${DST_PWD} dir:alpine_v2s1 docker://${DST_REPO}/alpine:v2s1
+if test $? -eq 0
+then
+  echo Pushed v2s1 image ${DST_REPO}/alpine:v2s1
+else
+  echo Failed to push v2s1 image ${DST_REPO}/alpine:v2s1
+  RET=1
+fi
+
+echo Pushing v2s2 image ${DST_REPO}/alpine:v2s2
+skopeo --insecure-policy copy --dest-tls-verify=false --dest-creds=${DST_USER}:${DST_PWD} dir:alpine_v2s2 docker://${DST_REPO}/alpine:v2s2
+if test $? -eq 0
+then
+  echo Pushed v2s2 image ${DST_REPO}/alpine:v2s2
+else
+  echo Failed to push v2s2 image ${DST_REPO}/alpine:v2s2
+  RET=1
+fi
+
+echo Pushing oci image ${DST_REPO}/alpine:oci
+skopeo --insecure-policy copy --dest-tls-verify=false --dest-creds=${DST_USER}:${DST_PWD} dir:alpine_oci docker://${DST_REPO}/alpine:oci
+if test $? -eq 0
+then
+  echo Pushed oci image ${DST_REPO}/alpine:oci
+else
+  echo Failed to push oci image ${DST_REPO}/alpine:oci
+  RET=1
+fi
+
+exit $RET
